b62342b0cd931b5744760227d7791e79a603551b
Amirjani <amir.jani500@gmail.com>
2021-08-30 11:47:09 +0430

    feature: ref and reactive values

--- composition/App.vue
+++ composition/App.vue
@@ -1,14 +1,41 @@
 <template>
-    <div>{{message}}</div>
+    <button @click="increment">{{ count }}</button>
+    <button @click="increase('a')">{{ numbers.a }}</button>
+    <button @click="increase('b')">{{ numbers.b }}</button>
 </template>
 
 <script>
+import { ref, reactive } from 'vue';
+
 export default {
     setup() {
-        return {
-            message: 'Hello World!'
+        // single values
+        const count = ref(0)
+
+        // multiple values
+        const numbers = reactive({
+            a: 0,
+            b: 0
+        })
+
+        const increment = () => {
+            count.value++;
         }
-    }
+
+        const increase = (number) => {
+            numbers[number]++;
+        }
+
+        return {
+            increment,
+            count,
+            numbers,
+            increase
+        }
+    },
+    created() {},
+    mounted() {},
+    
 }
 </script>
 
