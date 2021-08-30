821d81dd64a012779d67a20356ab25432d20aaac
Amirjani <amir.jani500@gmail.com>
2021-08-30 11:30:45 +0430

    feature: composition start

---
+++ composition/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>{{message}}</div>
+</template>
+
+<script>
+export default {
+    setup() {
+        return {
+            message: 'Hello World!'
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
